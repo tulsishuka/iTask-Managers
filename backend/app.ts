5c358d651d4e779f8e81f7fef90369c4540ec4da
Tulasi <tulsishuklag@gmail.com>
2026-05-05 22:51:21 +0530

    add

--- backend/app.ts
+++ backend/app.ts
@@ -1,48 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-
-// import authRouter from "./routes/authRouter";
-// import paymentRoutes from "./routes/paymentRoutes";
-// import userRoutes from "./routes/user.routes";
-// import charityRoutes from "./routes/charityRoutes";
-// import scoreRoutes from "./routes/scoreRoutes";
-// import drawRoutes from "./routes/drawRoutes";
-// import prizeRoutes from "./routes/prizeRoutes";
-// import resultRoutes from "./routes/resultRoutes";
-// import adminRoutes from "./routes/adminRoutes";
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use("/api/v1/auth", authRouter);
-// app.use("/api/payment", paymentRoutes);
-// app.use("/api/user", userRoutes);
-// app.use("/api/charity", charityRoutes);
-// app.use("/api/score", scoreRoutes);
-// app.use("/api/draw", drawRoutes);
-// app.use("/api/prize", prizeRoutes);
-// app.use("/api/results", resultRoutes);
-// app.use("/api/admin", adminRoutes);
-
-// export default app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import express from "express";
 import cors from "cors";
 
@@ -58,21 +13,9 @@ import adminRoutes from "./routes/adminRoutes";
 
 const app = express();
 
-// 🔥 Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-    
-      "https://givehope-platform-4.onrender.com" // 👉 change this
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 
-
-
-// 🔥 Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/user", userRoutes);
@@ -84,3 +27,16 @@ app.use("/api/results", resultRoutes);
 app.use("/api/admin", adminRoutes);
 
 export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
