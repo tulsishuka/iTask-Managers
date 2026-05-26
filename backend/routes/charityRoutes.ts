94d2885ea634e10b77ec8525328dfffebe3e50c6
Tulasi <tulsishuklag@gmail.com>
2026-05-26 15:21:33 +0530

    routes/charityRoutes.ts

--- backend/routes/charityRoutes.ts
+++ backend/routes/charityRoutes.ts
@@ -1,18 +1,4 @@
 
-// import express from "express";
-// import { protectedMiddleware } from "../middlewares/authMiddleware";
-// import { selectCharity, updateDonationPercentage } from "../controllers/charityController";
-
-// const router = express.Router();
-// router.post("/select", protectedMiddleware, selectCharity);
-
-// router.put(
-//   "/update-percentage",
-//   protectedMiddleware,
-//   updateDonationPercentage
-// );
-
-// export default router;
 import express from "express";
 import { protectedMiddleware } from "../middlewares/authMiddleware";
 import {
